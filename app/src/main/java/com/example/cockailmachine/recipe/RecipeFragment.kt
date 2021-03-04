0e3fe95642aabfbadd197987ee89367e3f7c81fa
gdufay <gdufay@student.42.fr>
2021-03-04 14:49:02 +0100

    Cleaning

--- app/src/main/java/com/example/cockailmachine/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/cockailmachine/recipe/RecipeFragment.kt
@@ -17,7 +17,7 @@ class RecipeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_recipe, container, false)
     }
 
 }
--- app/src/main/java/com/example/cockailmachine/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/cockailmachine/settings/SettingsFragment.kt
@@ -17,7 +17,7 @@ class SettingsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
 }
