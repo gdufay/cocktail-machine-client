69f9332fc116b568c7b746d09fcd71ed75b13ee0
gdufay <gdufay@student.42.fr>
2021-03-04 14:35:22 +0100

    Refactor default fragment and put them in their proper directory

--- app/src/main/java/com/example/cockailmachine/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/cockailmachine/settings/SettingsFragment.kt
@@ -1,17 +1,16 @@
-package com.example.cockailmachine
+package com.example.cockailmachine.settings
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import com.example.cockailmachine.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
