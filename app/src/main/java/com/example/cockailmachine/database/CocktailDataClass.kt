4326f499b5c1f2dd8828404cb0d1c6dd5dad6724
gdufay <gdufay@student.42.fr>
2021-03-18 16:30:03 +0100

    Update database with more constraints

--- app/src/main/java/com/example/cockailmachine/database/CocktailDataClass.kt
+++ app/src/main/java/com/example/cockailmachine/database/CocktailDataClass.kt
@@ -1,26 +1,45 @@
 package com.example.cockailmachine.database
 
-import android.graphics.Bitmap
-import androidx.room.*
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["name"], unique = true)])
 data class Cocktail(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    var name: String,
+    @NonNull var name: String,
     // @Ignore var picture: Bitmap? // Not used right now
 )
 
-@Entity
+@Entity(indices = [Index(value = ["name"], unique = true)])
 data class Ingredient(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    var name: String
+    @NonNull var name: String
 )
 
-@Entity(primaryKeys = ["cocktailId", "ingredientId"])
+@Entity(
+    primaryKeys = ["cocktailId", "ingredientId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = Cocktail::class,
+            parentColumns = ["id"],
+            childColumns = ["cocktailId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Ingredient::class,
+            parentColumns = ["id"],
+            childColumns = ["ingredientId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Quantity(
     val cocktailId: Int,
     val ingredientId: Int,
-    var quantity: Short,
+    @NonNull var quantity: Short,
 )
 
 /*
--- app/src/main/java/com/example/cockailmachine/database/CocktailDatabase.kt
+++ app/src/main/java/com/example/cockailmachine/database/CocktailDatabase.kt
@@ -1,7 +1,6 @@
 package com.example.cockailmachine.database
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -9,7 +8,7 @@ import androidx.room.RoomDatabase
 @Database(
     entities = [Cocktail::class, Ingredient::class, Quantity::class],
     version = 1,
-    exportSchema = false
+    exportSchema = true
 )
 abstract class CocktailDatabase : RoomDatabase() {
     abstract fun cocktailDao(): CocktailDao
