78e8fcfd3c0878e2c7c883cc672f3a9e8625ff4b
gdufay <gdufay@student.42.fr>
2021-03-19 19:19:55 +0100

    Improve recipe RecyclerView

--- app/src/main/java/com/example/cockailmachine/recipe/RecipeAdapter.kt
+++ app/src/main/java/com/example/cockailmachine/recipe/RecipeAdapter.kt
@@ -1,32 +1,50 @@
 package com.example.cockailmachine.recipe
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cockailmachine.R
-import com.example.cockailmachine.TextItemViewHolder
 import com.example.cockailmachine.database.Cocktail
 
-class RecipeAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class RecipeAdapter : RecyclerView.Adapter<RecipeAdapter.ViewHolder>() {
     var data = listOf<Cocktail>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.recipe_item_view, parent, false) as TextView
-
-        return TextItemViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
 
-        holder.textView.text = item.name
+        holder.bind(item)
     }
 
+
     override fun getItemCount() = data.size
+
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val cocktailImage: ImageView = itemView.findViewById(R.id.cocktail_img)
+        private val cocktailName: TextView = itemView.findViewById(R.id.cocktail_name)
+
+        fun bind(item: Cocktail) {
+            cocktailName.text = item.name
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.recipe_item_view, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
+    }
+
 }
