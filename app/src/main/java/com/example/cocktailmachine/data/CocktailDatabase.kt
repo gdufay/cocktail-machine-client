65f590dd35c0a77ee290b71745811e3e2696dfe9
gdufay <gdufay@student.42.fr>
2021-04-10 17:11:38 +0200

    Prepopulate db

--- app/src/main/java/com/example/cocktailmachine/data/CocktailDatabase.kt
+++ app/src/main/java/com/example/cocktailmachine/data/CocktailDatabase.kt
@@ -5,16 +5,69 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.cocktailmachine.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(
     entities = [Cocktail::class, Ingredient::class, Quantity::class],
     version = 1,
-    exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class CocktailDatabase : RoomDatabase() {
+
     abstract fun cocktailDao(): CocktailDao
     abstract fun ingredientDao(): IngredientDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<CocktailDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val cocktailDao = database.get().cocktailDao()
+            val ingredientDao = database.get().ingredientDao()
+
+            val ingredients = listOf("bière", "eau", "rhum", "vodka", "sirop de grenadine")
+            val cocktails = listOf(
+                Pair(
+                    "mojito",
+                    "https://st.depositphotos.com/1367205/3711/i/950/depositphotos_37116235-stock-photo-mojito-cocktail.jpg"
+                ),
+            )
+            val quantities = listOf<Triple<Int, Int, Short>>(
+                Triple(1, 3, 4)
+            )
+
+            applicationScope.launch {
+                // insert ingredient
+                for (ingredient in ingredients)
+                    ingredientDao.insertIngredient(Ingredient(ingredientName = ingredient))
+
+                for (cocktail in cocktails)
+                    cocktailDao.insertCocktail(
+                        Cocktail(
+                            cocktailName = cocktail.first,
+                            cocktailUri = Uri.parse(cocktail.second)
+                        )
+                    )
+
+                for (quantity in quantities)
+                    ingredientDao.insertQuantity(
+                        Quantity(
+                            cocktailId = quantity.first,
+                            ingredientId = quantity.second,
+                            quantity = quantity.third
+                        )
+                    )
+            }
+        }
+    }
 }
 
 // TODO: move
--- app/src/main/java/com/example/cocktailmachine/di/DatabaseModule.kt
+++ app/src/main/java/com/example/cocktailmachine/di/DatabaseModule.kt
@@ -2,37 +2,43 @@ package com.example.cocktailmachine.di
 
 import android.content.Context
 import androidx.room.Room
-import com.example.cocktailmachine.data.CocktailDao
 import com.example.cocktailmachine.data.CocktailDatabase
-import com.example.cocktailmachine.data.IngredientDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
-@InstallIn(SingletonComponent::class)
 @Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext appContext: Context): CocktailDatabase {
-        return Room.databaseBuilder(
-            appContext,
-            CocktailDatabase::class.java,
-            "cocktail_database"
-        ).build()
-    }
+    fun provideDatabase(
+        @ApplicationContext appContext: Context,
+        callback: CocktailDatabase.Callback
+    ) = Room.databaseBuilder(appContext, CocktailDatabase::class.java, "cocktail_database")
+        .fallbackToDestructiveMigration()
+        .addCallback(callback)
+        .build()
 
     @Provides
-    fun provideCocktailDao(database: CocktailDatabase): CocktailDao {
-        return database.cocktailDao()
-    }
+    fun provideCocktailDao(database: CocktailDatabase) = database.cocktailDao()
 
     @Provides
-    fun provideIngredientDao(database: CocktailDatabase): IngredientDao {
-        return database.ingredientDao()
-    }
+    fun provideIngredientDao(database: CocktailDatabase) = database.ingredientDao()
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
